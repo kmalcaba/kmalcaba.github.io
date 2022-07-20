dbe5cc31980a4468843d63fad0cf8001a4518feb
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-20 16:21:07 -0700

    Add header component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
+import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
 import "./Home.scss";
 
 function Home() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+      <Header title="PROJECTS" subHeader={true} />
+    </>
   );
 }
 export default Home;
