92d1cca5bd6c2e8c46c6e7fc05f64b8ddb63d3b9
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-20 16:28:38 -0700

    Import new components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import Projects from "../../components/Projects/Projects";
 import "./Home.scss";
 
 function Home() {
   return (
     <>
       <Hero />
-      <Header title="PROJECTS" subHeader={true} />
+      <Header title="FEATURED PROJECTS" subHeader={true} />
+      <Projects>
+        <ProjectCard />
+      </Projects>
     </>
   );
 }
