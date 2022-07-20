53c1dcb2b2a81c6a9dce992560f7e2a0f9327494
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-20 14:45:03 -0700

    Reset development

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div>
+      <div className="title">KIRSTEN</div>
+      <nav className="nav">
+        <ul>
+          <li>ABOUT</li>
+          <li>PROJECTS</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Header;
--- src/components/styled/MainContainer.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const MainContainer = styled.div`
-  width: 1440px;
-  max-width: 100%;
-  padding: 0 165px;
-  margin: 0 auto;
-`;
