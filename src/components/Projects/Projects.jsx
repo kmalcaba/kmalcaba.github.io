56e707ac712a8f92a2e31e956dfbacb2d9e913fb
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-20 17:06:59 -0700

    Add basic skeleton and styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,15 @@
 import "./Projects.scss";
+import ProjectCard from "../ProjectCard/ProjectCard";
 
-function Projects({ children }) {
-  return <div>{children}</div>;
+function Projects() {
+  return (
+    <section className="projects">
+      {
+        // TODO: Add filtering logic here
+      }
+      <ProjectCard />
+      <ProjectCard />
+    </section>
+  );
 }
 export default Projects;
