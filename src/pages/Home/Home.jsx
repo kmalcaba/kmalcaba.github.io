7a1f7a6f6127d72070fc65aff3856389a9bed1ce
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-20 17:07:27 -0700

    Add projects component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
-import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import Projects from "../../components/Projects/Projects";
 import "./Home.scss";
 
@@ -9,9 +8,7 @@ function Home() {
     <>
       <Hero />
       <Header title="FEATURED PROJECTS" subHeader={true} />
-      <Projects>
-        <ProjectCard />
-      </Projects>
+      <Projects />
     </>
   );
 }
