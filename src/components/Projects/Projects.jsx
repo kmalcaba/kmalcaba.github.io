0bb4676e65e9a804b0baaeb145262406efbf424c
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-20 17:18:55 -0700

    Create dummy project variable and prop

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,13 +2,22 @@ import "./Projects.scss";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
 function Projects() {
+  const project = {
+    title: "Project Title",
+    tags: ["ReactJS", "HTML/CSS"],
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium officiis qui ad consectetur quos dolorem consequatur exercitationem fugit debitis repudiandae placeat aperiam, iste animi laboriosam! Porro laborum vel possimus recusandae?",
+    previewLink: "/",
+    sourceLink: "/",
+  };
+
   return (
     <section className="projects">
       {
         // TODO: Add filtering logic here
       }
-      <ProjectCard />
-      <ProjectCard />
+      <ProjectCard project={project} />
+      <ProjectCard project={project} />
     </section>
   );
 }
