8c9b9975ab50e885b2058c840a6ba0090280edea
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-20 18:33:21 -0700

    Add header nav responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,71 @@
 import "./Header.scss";
+import { RiMenuFill, RiCloseFill } from "react-icons/ri";
+import { useState, useEffect } from "react";
 
 function Header({ title = "KIRSTEN", subHeader = false }) {
+  const [isOpen, setOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && isOpen) {
+      isOpen(false);
+    }
+  }, [size.width, isOpen]);
+
+  const toggleHandler = () => {
+    setOpen((previousState) => !previousState);
+  };
+
   return (
     <header className="header">
       <div className="header-title">{title}</div>
-      <nav className="header-nav">
-        {!subHeader && (
-          <ul>
-            <li>
-              <a href="/">ABOUT</a>
-            </li>
-            <li>
-              <a href="/">PROJECTS</a>
-            </li>
-            <li>
-              <a href="/">CONTACT</a>
-            </li>
-          </ul>
-        )}
-      </nav>
+      {!subHeader && (
+        <>
+          <nav
+            className={`header-nav ${
+              isOpen && size.width < 540 && `mobile-menu`
+            }`}
+          >
+            <ul>
+              <li>
+                <a href="/">ABOUT</a>
+              </li>
+              <li>
+                <a href="/">PROJECTS</a>
+              </li>
+              <li>
+                <a href="/">CONTACT</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="header-toggle">
+            {!isOpen ? (
+              <RiMenuFill onClick={toggleHandler} />
+            ) : (
+              <RiCloseFill className="close" onClick={toggleHandler} />
+            )}
+          </div>
+        </>
+      )}
     </header>
   );
 }
