bf6e746d7a61c5b658ee99e2d2632bb69c337ded
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-04 14:37:52 -0700

    Implement tags filtering

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,30 +2,94 @@ import "./Projects.scss";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import { ContainerWrapper, MotionWrapper } from "../../wrapper";
 import { images } from "../../constants";
+
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+const TAGS_ARRAY = [
+  "ReactJS",
+  "HTML/CSS",
+  "JavaScript",
+  "SCSS",
+  "styled-components",
+  "MERN",
+  "All",
+];
 
 function Projects() {
-  const project = {
-    title: "Project Title",
-    tags: ["ReactJS", "HTML/CSS"],
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium officiis qui ad consectetur quos dolorem consequatur exercitationem fugit debitis repudiandae placeat aperiam, iste animi laboriosam! Porro laborum vel possimus recusandae?",
-    image: images.about01,
-    previewLink: "/",
-    sourceLink: "/",
+  const [projects, setProjects] = useState([
+    {
+      title: "Project Title",
+      tags: ["ReactJS", "SCSS"],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium officiis qui ad consectetur quos dolorem consequatur exercitationem fugit debitis repudiandae placeat aperiam, iste animi laboriosam! Porro laborum vel possimus recusandae?",
+      image: images.about01,
+      previewLink: "/",
+      sourceLink: "/",
+    },
+    {
+      title: "Project 2",
+      tags: ["JavaScript", "HTML/CSS"],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium officiis qui ad consectetur quos dolorem consequatur exercitationem fugit debitis repudiandae placeat aperiam, iste animi laboriosam! Porro laborum vel possimus recusandae?",
+      image: images.about02,
+      previewLink: "/",
+      sourceLink: "/",
+    },
+    {
+      title: "Project 3",
+      tags: ["JavaScript", "ReactJS"],
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium officiis qui ad consectetur quos dolorem consequatur exercitationem fugit debitis repudiandae placeat aperiam, iste animi laboriosam! Porro laborum vel possimus recusandae?",
+      image: images.about03,
+      previewLink: "/",
+      sourceLink: "/",
+    },
+  ]);
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  const handleFilter = (filter) => {
+    setActiveFilter(filter);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
   };
 
-  return (
-    <div>
-      {
-        // TODO: Add filtering logic here
+  useEffect(() => {
+    setFilteredProjects(projects);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (activeFilter === "All") {
+        setFilteredProjects(projects);
+      } else {
+        setFilteredProjects(
+          projects.filter((project) => project.tags.includes(activeFilter))
+        );
       }
+    }, 500);
+  }, [activeFilter, projects]);
+
+  return (
+    <div className="project">
+      <ul className="project-tags">
+        {TAGS_ARRAY.map((item) => (
+          <li key={`filter-${item}`} onClick={() => handleFilter(item)}>
+            {item}
+          </li>
+        ))}
+      </ul>
       <motion.div
-        transition={{ duration: 0.3, delayChildren: 0.3, staggerChildren: 0.3 }}
+        animate={animateCard}
+        transition={{ duration: 0.3, delayChildren: 0.3, staggerChildren: 0.5 }}
         className="project-cards"
       >
-        <ProjectCard project={project} />
-        <ProjectCard project={project} />
+        {filteredProjects.map((project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
       </motion.div>
     </div>
   );
