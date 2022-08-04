c77921f40a798da316b27d2f1d35b5b94be2856f
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-04 15:55:37 -0700

    Add social media links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,39 @@
 import "./Contact.scss";
+import Button from "../Button/Button";
 import { ContainerWrapper, MotionWrapper } from "../../wrapper";
+
+import { AiFillMail, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 function Contact() {
   return (
     <div className="contact">
       <h2>
-        Get in touch, drop me an <a href="mailto:kmalcaba@live.com">email</a>!
+        Get in touch, drop me an{" "}
+        <a className="contact-link" href="mailto:kmalcaba@live.com">
+          email
+        </a>
+        !
       </h2>
       <div className="contact-cards">
-        <div className="contact-card"></div>
+        <a href="mailto:kmalcaba@live.com">
+          <Button className="btn-primary" Icon={AiFillMail}>
+            Email
+          </Button>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kmalcaba/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Button className="btn-primary" Icon={AiFillLinkedin}>
+            LinkedIn
+          </Button>
+        </a>
+        <a href="https://github.com/kmalcaba" target="_blank" rel="noreferrer">
+          <Button className="btn-primary" Icon={AiFillGithub}>
+            Github
+          </Button>
+        </a>
       </div>
     </div>
   );
