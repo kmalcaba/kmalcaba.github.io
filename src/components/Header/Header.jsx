f548212c1f5e390a73d69eaebbdfa7860b48d87c
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-03 17:23:03 -0700

    Rebuild mobile navigation menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,26 @@
 import "./Header.scss";
 import { RiMenuFill, RiCloseFill } from "react-icons/ri";
 import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const NAVBAR_LINKS = ["about", "projects", "contact"];
+
+const variants = {
+  enter: {
+    x: 500,
+    opacity: 0,
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: {
+    zIndex: 0,
+    x: 500,
+    opacity: 0,
+  },
+};
 
 function Header({ title = "KIRSTEN", subHeader = false }) {
   const [isOpen, setOpen] = useState(false);
@@ -35,38 +55,52 @@ function Header({ title = "KIRSTEN", subHeader = false }) {
   };
 
   return (
-    <header className="header">
+    <nav className="header">
       <div className="header-title">{title}</div>
       {!subHeader && (
         <>
-          <nav
-            className={`header-nav ${
-              isOpen && size.width < 540 && `mobile-menu`
-            }`}
-          >
+          <div className="header-nav">
             <ul>
-              <li>
-                <a href="/">ABOUT</a>
-              </li>
-              <li>
-                <a href="/">PROJECTS</a>
-              </li>
-              <li>
-                <a href="/">CONTACT</a>
-              </li>
+              {NAVBAR_LINKS.map((item) => (
+                <li key={`link-${item}`}>
+                  <a href={`#${item}`}>{item}</a>
+                </li>
+              ))}
             </ul>
-          </nav>
-
-          <div className="header-toggle">
-            {!isOpen ? (
-              <RiMenuFill onClick={toggleHandler} />
-            ) : (
-              <RiCloseFill className="close" onClick={toggleHandler} />
-            )}
           </div>
+          <div className="header-nav-mobile">
+            <RiMenuFill onClick={toggleHandler} />
+
+            <AnimatePresence initial={false}>
+              {isOpen && (
+                <motion.div
+                  key="header-nav-mobile"
+                  variants={variants}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{
+                    x: { type: "spring", stiffness: 300, damping: 30 },
+                    opacity: { duration: 0.2 },
+                  }}
+                >
+                  <RiCloseFill onClick={toggleHandler} />
+                  <ul>
+                    {NAVBAR_LINKS.map((item) => (
+                      <li key={item}>
+                        <a href={`#${item}`} onClick={toggleHandler}>
+                          {item}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>{" "}
         </>
       )}
-    </header>
+    </nav>
   );
 }
 export default Header;
