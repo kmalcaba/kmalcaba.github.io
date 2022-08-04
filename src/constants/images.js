0e857b5b974737ac9b58754ce2ff96b38960e887
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-03 18:28:57 -0700

    Upload assets

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import about01 from "../assets/about01.png";
+import about02 from "../assets/about02.png";
+import about03 from "../assets/about03.png";
+import about04 from "../assets/about04.png";
+
+export default {
+  about01,
+  about02,
+  about03,
+  about04,
+};
