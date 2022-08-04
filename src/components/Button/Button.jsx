037bb56d0d5de1bac277c455f7315345822d0587
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-04 15:55:04 -0700

    Add onClick method

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import "./Button.scss";
 import React from "react";
 
-function Button({ className = "", Icon = React.Fragment, children }) {
+function Button({ className = "", Icon = React.Fragment, onClick, children }) {
   return (
-    <button className={className}>
+    <button className={className} onClick={onClick}>
       <Icon /> {children}
     </button>
   );
